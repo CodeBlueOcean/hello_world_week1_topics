aaeffcaa27589c3dff8e336642133195848acc79
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-01-28 23:06:23 -0600

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,42 @@
+"""
+Declaring Variables
+"""
+
+x = 12.2
+y = 7
+z = 9.0
+
+print(x)
+print(y)
+print(z)
+
+"""
+Primitive Data types
+"""
+
+name = "Bob"  # Storing a String Value
+age = 35  # Storing an Integer value
+cash = 100.25  # Storing a float value
+retired = False # Storing a Boolean value
+
+# How to known the Data type of a Variable
+# Invoking the function 'type(<VARIABLE NAME>)'
+print("Data Type of the variable 'name' is", type(name))
+print("Data Type of the variable 'age' is", type(age))
+print("Data Type of the variable 'cash' is", type(cash))
+print("Data Type of the variable 'retired is", type(retired))
+
+"""
+Composite Data types
+"""
+nucamp_locations = ["Seattle", "Tacoma", "Bellevue"]
+Bob_Info = {"name": "Bob", "age": 35, "cash": 100.25, "retired": False}
+my_tuple = ("apple","banana","cherry")
+my_set = {"cats","dogs","birds"}
+
+print("Data Type of the variable 'nucamp_locations' is", type(nucamp_locations))
+print("Data Type of the variable 'Bob' is", type(Bob_Info))
+print("Data Type of the variable 'my_tuple' is", type(my_tuple))
+print("Data Type of the variable 'my_set' is", type(my_set))
+
+
---
+++ week1.py
@@ -0,0 +1,32 @@
+# Review: Primitive Data Types
+"""
+int
+float
+string
+boolean
+"""
+"""
+4 primary composite data types 
+list: [] square brackets ordered name 
+dict: {} unordered name with key value
+tuple: () tuple you can't repeat the same value, you can't edit imutable. (ancidentally rewrite)
+set: 
+"""
+
+"""
+Review of the Input Function
+
+The input is used to collect keyboard inputs from the user
+It is a function (more in depth next week on functions)
+    userInput = input("Hello!")
+
+print(userInput) // Null
+
+The input function will Always return a string
+
+    age = int(input('How old are you?'))
+    input = prompt(JS) == std::cin >> (C++)
+    print(int(age))
+    print(age) # '7' -> string
+
+"""
